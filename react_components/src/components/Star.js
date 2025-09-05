0518f21c3b7a175b1ed595f02f3b18821980b98c
PogozhevIvan18 <pogozhev_reserv@mail.ru>
2025-09-06 00:14:02 +0400

    new rating

--- react_components/src/components/Star.js
+++ react_components/src/components/Star.js
@@ -2,8 +2,8 @@ import React from "react";
 import {FaStar} from "react-icons/fa"
 import colorArray from "./UI/color"
 
-function Star({selected = false, onSelect = f => f, colorValue = 0}) {
-    return (<FaStar color={selected ? colorArray[colorValue - 1] : "grey"} onClick={onSelect}/>)
+function Star({selected = false, onSelect = f => f, index}) {
+    return (<FaStar color={selected ? colorArray[index] : "grey"} onClick={onSelect}/>)
 };
 
 export default Star;
--- react_components/src/components/StarRating.js
+++ react_components/src/components/StarRating.js
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, {useState } from "react";
 import Star from "./Star";
 
-
-
 export default function StarRating() {
-    const [selectedStars,setSelectedStars] = useState(0)
+    const originArrayColor = [1, 2, 3, 4, 5]
+    const [selectedStars,setSelectedStars] = useState([])
     const createArray = length => [...Array(length)]
+    
+
+    const clickColor = (index) => {  
+        const newColor = originArrayColor.slice(0, index)
+        if(newColor.length < selectedStars.length) {
+            setSelectedStars(newColor)
+        } else {
+            const newColor = originArrayColor.slice(0, index)
+            setSelectedStars(newColor)
+        }   
+    }
     return (
         <>
-        {createArray(5).map((n, i) => (
-            <Star key={i} selected={selectedStars > i} colorValue={selectedStars} onSelect={() => setSelectedStars(i + 1)}/>))}
+        {createArray(5).map((n, index) => (
+            <Star key={index} index={index} selected={selectedStars.length > index} onSelect={() => clickColor(index + 1)}/>
+            ))  
+        }
         <p>
-            {selectedStars} из 5 звезд
+            {selectedStars.length} из 5 звезд 
         </p>
-        </>
+        </>   
     )
 };
